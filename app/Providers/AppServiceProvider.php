06f9ff2ee9a0d0ed3a7a39196b647347f2e0040e
Vladimir Ković <vlada.kovic@gmail.com>
2019-05-12 19:59:25 +0200

    Load environment specific env file TR-N678tJ8w

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Flugg\Responder\Contracts\Transformers\TransformerResolver;
 use Illuminate\Database\Eloquent\FactoryBuilder;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,12 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Ability to disable model events when using factories
-        FactoryBuilder::macro('withoutEvents', function () {
-            $this->class::flushEventListeners();
+        $this->loadEnvironmentSpecificEnvFile();
 
-            return $this;
-        });
+        $this->addWithoutEventsMacroToFactory();
     }
 
     /**
@@ -31,6 +29,38 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() === 'local') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+    }
+
+    /**
+     * Provide way to disable model events when using factories
+     *
+     * @return void
+     */
+    protected function addWithoutEventsMacroToFactory()
+    {
+        FactoryBuilder::macro('withoutEvents', function () {
+            $this->class::flushEventListeners();
+
+            return $this;
+        });
+    }
+
+    /**
+     * Try to load additional .env file if we're not on production
+     *
+     * @return void
+     */
+    protected function loadEnvironmentSpecificEnvFile()
+    {
+        if (!\App::environment('production')) {
+            $basePath = base_path();
+            $envFile = '.' . \App::environment() . '.env';
+
+            if (file_exists($basePath . DIRECTORY_SEPARATOR . $envFile)) {
+                // Finally load env specific env file
+                (new \Dotenv\Dotenv(base_path(), $envFile))->overload();
+            }
         }
     }
 }
